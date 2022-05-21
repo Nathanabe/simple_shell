397b9ed0e58a221c34dacd138a14aa23335c489d
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2022-05-21 05:39:51 +0100

    its the numbers for now

---
+++ is_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_number - checks if a string is a number
+ * @s: the string
+ *
+ * Return: 1 if it is a number, 0 otherwise
+ */
+int is_number(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] < 48 || s[i] > 57)
+			return (0);
+	return (1);
+}
