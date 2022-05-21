2b9ce9d067e5827ca1a7ae3fa73ab5a0ffd5af08
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2022-05-21 04:59:22 +0100

    getline.c commit

---
+++ _getline.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _getline - reads one line from stdin
+ * @buf: the buffer for storing the line
+ * @sz: the number of bytes to be read
+ *
+ * Return: the number of bytes read
+ */
+ssize_t _getline(char *buf, size_t sz)
+{
+	int fd;
+	ssize_t nchr = 0;
+	ssize_t idx = 0;
+	char *p = NULL;
+
+	fd = STDIN_FILENO;
+	/* position fd & read line */
+	nchr = read(fd, buf, sz);
+	if (nchr == -1)
+	{   /* read error   */
+		perror("Error: Couldn't read from stdin");
+		return (nchr);
+	}
+	/* end of file - no chars read (not an error, but return -1)*/
+	if (nchr == 0)
+		return (-1);
+	p = buf;    /* check each char */
+	while (idx < nchr)
+	{
+		p++;
+		idx++;
+	}
+	*p = 0;
+	return (idx);
+}
